61fa240d3ab372f0182d1ac4754f08cb67375212
xinsanic <xinsanic@mendelu.cz>
2023-12-01 12:22:01 +0100

    added flask

---
+++ app/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return '<a href="http://127.0.0.1:5000/home">Dovážková služba</a>'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
