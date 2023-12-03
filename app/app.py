aa83fb94e84b93b8dcc73950a0d5b5351025cdb9
xinsanic <xinsanic@mendelu.cz>
2023-12-03 18:41:37 +0100

    fixed css cause it stopped working

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_folder='../static/', static_url_path='')
 
 @app.route("/")
 def index():
-    return '<a href="http://127.0.0.1:5000/html/">Dovážková služba</a>'
+    return '<a href="http://127.0.0.1:5000/html/index.html">Dovážková služba</a>'
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
