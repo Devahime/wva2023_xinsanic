e00fca8e0881a1e66209bd9fff690f1d7ead9828
xinsanic <xinsanic@mendelu.cz>
2023-12-03 18:14:04 +0100

    fixed index.html and app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../static/', static_url_path='')
 
 @app.route("/")
 def index():
-    return '<a href="http://127.0.0.1:5000/index">Dovážková služba</a>'
+    return '<a href="http://127.0.0.1:5000/html/">Dovážková služba</a>'
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
