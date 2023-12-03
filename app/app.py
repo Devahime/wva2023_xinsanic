895a50b6a7450d0514355301d7d4f1b641f7efb5
xinsanic <xinsanic@mendelu.cz>
2023-12-03 17:57:53 +0100

    moved folders, created new ones

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<a href="http://127.0.0.1:5000/home">Dovážková služba</a>'
+    return '<a href="http://127.0.0.1:5000/index">Dovážková služba</a>'
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
