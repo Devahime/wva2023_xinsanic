94d6278a666108b5ce95e8e5205a92b71fc438b0
xmichen1 <xmichen1@mendelu.cz>
2024-01-12 18:43:10 +0100

    render_template

--- app/app.py
+++ app/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from database import database
+import os
 
-app = Flask(__name__, static_folder='../static/', static_url_path='')
+app = Flask(__name__, static_folder='static', static_url_path='/static')
+
+database.init_app(app)
 
 @app.route("/")
 def index():
-    return '<a href="http://127.0.0.1:5000/html/index.html">Dovážková služba</a>'
+    #return '<a href="http://127.0.0.1:5000/html/index.html">Dovážková služba</a>'
+    return render_template('/html/index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
